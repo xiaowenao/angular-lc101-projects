262075501205e36aa101d511f65241ffee7280c9
Xiaowen Ao <xao@stumail.jccc.edu>
2020-09-21 18:49:11 -0500

    Angular part1 lesson finished

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Here are my favoriate photos for 2020';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://cdn1.parksmedia.wdprapps.disney.com/media/blog/wp-content/uploads/2020/06/u4yu8yiu3861323-1024x576.jpg';
+  image3 = 'https://cdn1.parksmedia.wdprapps.disney.com/media/blog/wp-content/uploads/2020/06/u4yu8yiu3861324-1024x576.jpg';
 
   constructor() { }
 
